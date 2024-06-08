50d99e882ca83964769cbf64b0be0911325f046b
vishva7 <vishvamehta301@gmail.com>
2024-06-08 18:47:58 +0530

    added functions to get data from backend

--- conect/src/app/people/page.tsx
+++ conect/src/app/people/page.tsx
@@ -1,31 +1,49 @@
+"use client"
+import { useEffect,useState } from "react";
+import axios from "axios";
 import React from "react";
 import MemberCard from "@/components/memberCard";
 import { details } from "@/lib/types";
 import Navbar from "@/components/navbar";
 
-const someData: details[] = [
-  {
-    name: "Bivas Bhattacharya",
-    role: "Assistant Professor",
-    image: "",
-    desc: "Course Anchorship, Lab in-charge, Project Guide, Class teacher, Student mentor, Club advisor",
-  },
-  {
-    name: "Alejandro Gomez",
-    role: "Assistant Professor",
-    image: "",
-    desc: "A passionate advocate for interdisciplinary research.",
-  },
-];
 
+// const someData: details[] = [
+//   {
+//     name: "Bivas Bhattacharya",
+//     role: "Assistant Professor",
+//     image: "",
+//     desc: "Course Anchorship, Lab in-charge, Project Guide, Class teacher, Student mentor, Club advisor",
+//   },
+//   {
+//     name: "Alejandro Gomez",
+//     role: "Assistant Professor",
+//     image: "",
+//     desc: "A passionate advocate for interdisciplinary research.",
+//   },
+// ];
+interface people {
+  name: string;
+  role: string;
+  image: string;
+  desc: string;
+}
 export default function About() {
+
+  const [about, setAbout] = useState<people[]>([]);
+  useEffect(()=>{
+    axios.get("http://localhost:10000/people/all")
+    .then(response => {
+      setAbout(response.data);})
+    .catch(err => console.log(err))
+  },[])
+
   return (
     <>
       <Navbar />
       <div className="container">
         <h1>Our Team</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {someData.map((member, index) => (
+          {about.map((member, index) => (
             <MemberCard key={index} {...member} />
           ))}
         </div>
--- conect/src/app/publications/page.tsx
+++ conect/src/app/publications/page.tsx
@@ -1,31 +1,50 @@
+"use client"
+import { useEffect,useState } from "react";
+import axios from "axios";
 import PublicationCard from "@/components/publicationCard";
 import Navbar from "@/components/navbar";
 
-const publications = [
-  {
-    title: "Understanding the Impact of Climate Change",
-    authors: "By Dr. A. Smith, Prof. A. Johnson, Dr. K. Lee",
-    summary:
-      "This study examines the regional effects of climate change on agricultural productivity and proposes adaptive strategies for farmers.",
-    link: "#",
-  },
-  {
-    title: "Innovations in Renewable Energy Technologies",
-    authors: "By Prof. J. Garcia, Dr. S. Patel, Prof. M. Chen",
-    summary:
-      "This paper presents the latest advancements in solar, wind, and hydroelectric power technologies, highlighting breakthroughs in energy efficiency and cost reduction.",
-    link: "#",
-  },
-  {
-    title: "The Future of Urban Mobility: Smart Cities and Transportation",
-    authors: "By Dr. L. Wang, Prof. C. Brown, Dr. E. Martinez",
-    summary:
-      "This research explores the potential of smart city technologies to revolutionize transportation systems, analyzing the impact on traffic management, public transportation, and the adoption of electric and autonomous vehicles.",
-    link: "#",
-  },
-];
+// const publications = [
+//   {
+//     title: "Understanding the Impact of Climate Change",
+//     authors: "By Dr. A. Smith, Prof. A. Johnson, Dr. K. Lee",
+//     summary:
+//       "This study examines the regional effects of climate change on agricultural productivity and proposes adaptive strategies for farmers.",
+//     link: "#",
+//   },
+//   {
+//     title: "Innovations in Renewable Energy Technologies",
+//     authors: "By Prof. J. Garcia, Dr. S. Patel, Prof. M. Chen",
+//     summary:
+//       "This paper presents the latest advancements in solar, wind, and hydroelectric power technologies, highlighting breakthroughs in energy efficiency and cost reduction.",
+//     link: "#",
+//   },
+//   {
+//     title: "The Future of Urban Mobility: Smart Cities and Transportation",
+//     authors: "By Dr. L. Wang, Prof. C. Brown, Dr. E. Martinez",
+//     summary:
+//       "This research explores the potential of smart city technologies to revolutionize transportation systems, analyzing the impact on traffic management, public transportation, and the adoption of electric and autonomous vehicles.",
+//     link: "#",
+//   },
+// ];
+
+interface publications {
+  title: string;
+  authors: string;
+  summary: string;
+  link: string;
+}
 
 export default function Publications() {
+
+  const [publications, setPublications] = useState<publications[]>([]);
+  useEffect(()=>{
+    axios.get("http://localhost:10000/publications/all")
+    .then(response => {
+      setPublications(response.data);})
+    .catch(err => console.log(err))
+  },[])
+
   return (
     <div>
       <Navbar />
