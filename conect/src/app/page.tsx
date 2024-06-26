45060b261a42d7153db5ef616d6dcc5dcb430580
VyoJ <75789232+VyoJ@users.noreply.github.com>
2024-06-26 11:14:47 +0530

    Home page fixes

--- conect/src/app/page.tsx
+++ conect/src/app/page.tsx
@@ -1,64 +1,5 @@
 "use client";
 
-// import CarouselItem from "@/components/gallery";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from "react-responsive-carousel";
-// import Navbar from "@/components/navbar";
-// import { useState, useEffect } from "react";
-// import { CarouselData } from "@/lib/types";
-// import axios from "axios";
-
-// const Home = () => {
-//   const [carouselItemsData, setCarouselItemsData] = useState<CarouselData[]>(
-//     []
-//   );
-
-//   useEffect(() => {
-//     axios
-//       .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/home/all`)
-//       .then((response) => {
-//         let data = response.data.map((item: CarouselData) => {
-//           const matchResult = item.image.match(/file\/d\/(.*?)\//);
-//           if (matchResult) {
-//             const fileId = matchResult[1];
-//             const newImageUrl = `https://drive.google.com/uc?export=view&id=${fileId}`;
-//             return { ...item, image: newImageUrl };
-//           } else {
-//             return item;
-//           }
-//         });
-//         setCarouselItemsData(data);
-//       })
-//       .catch((err) => {
-//         console.error("Failed to fetch carousel items:", err);
-//       });
-//   }, []);
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="lg:h-[calc(100vh-150px)] flex w-full items-center justify-center">
-//         <Carousel
-//           showArrows={false}
-//           autoPlay
-//           infiniteLoop
-//           swipeable
-//           showStatus={false}
-//           emulateTouch
-//           showThumbs={false}
-//           interval={3500}
-//         >
-//           {carouselItemsData.map((item: CarouselData, index) => (
-//             <CarouselItem key={index} {...item} />
-//           ))}
-//         </Carousel>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Home;
-
 import { useEffect, useState } from "react";
 import CarouselItem from "@/components/gallery";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -71,32 +12,37 @@ const Home = () => {
   const [carouselItemsData, setCarouselItemsData] = useState<CarouselData[]>(
     []
   );
+  const [fetchCount, setFetchCount] = useState(0); // State to track fetch count
 
   useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/home/all`)
-      .then((response) => {
-        const data = response.data.map((item: CarouselData) => {
-          const matchResult = item.image.match(/file\/d\/(.*?)\//);
-          if (matchResult) {
-            const fileId = matchResult[1];
-            const newImageUrl = `https://drive.google.com/uc?export=view&id=${fileId}`;
-            return { ...item, image: newImageUrl };
-          } else {
-            return item;
-          }
+    if (fetchCount < 3) {
+      // Limit fetch to two times
+      axios
+        .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/home/all`)
+        .then((response) => {
+          const data = response.data.map((item: CarouselData) => {
+            const matchResult = item.image.match(/file\/d\/(.*?)\//);
+            if (matchResult) {
+              const fileId = matchResult[1];
+              const newImageUrl = `https://drive.google.com/uc?export=view&id=${fileId}`;
+              return { ...item, image: newImageUrl };
+            } else {
+              return item;
+            }
+          });
+          setCarouselItemsData(data);
+          setFetchCount(fetchCount + 1); // Increment fetch count
+        })
+        .catch((err) => {
+          console.error("Failed to fetch carousel items:", err);
         });
-        setCarouselItemsData(data);
-      })
-      .catch((err) => {
-        console.error("Failed to fetch carousel items:", err);
-      });
-  }, []);
+    }
+  }, [fetchCount]);
 
   return (
     <>
       <Navbar />
-      <div style={{ minHeight: "calc(100vh - 150px)" }}>
+      <div style={{ height: "calc(100vh - 150px)" }}>
         <Carousel
           showArrows={false}
           autoPlay
