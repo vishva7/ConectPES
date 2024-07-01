7300a7d19d7a6d9757cd1b39eca3df70ea8d8cce
VyoJ <75789232+VyoJ@users.noreply.github.com>
2024-07-01 11:50:26 +0530

    People image fix

--- conect/src/components/memberCard.tsx
+++ conect/src/components/memberCard.tsx
@@ -1,23 +1,29 @@
-"use client"
+"use client";
 
 import { Card } from "./ui/Card";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { details } from "@/lib/types";
+import Image from "next/image";
 
-export default function MemberCard({ name, role, image, description }: details) {
+export default function MemberCard({
+  name,
+  role,
+  image,
+  description,
+}: details) {
   return (
     <Card className="w-full max-w-sm rounded-xl border">
       <div className="p-6 grid gap-4">
         <div className="flex gap-8 items-center">
-          <Avatar>
-            <AvatarImage src={image} className=""/>
-            <AvatarFallback>{name[0]}</AvatarFallback>
-          </Avatar>
+          <Image
+            src={image}
+            width={64}
+            height={64}
+            className="w-16 h-16 rounded-full"
+            alt={name[0]}
+          />
           <div className="grid gap-1.5">
             <h2 className="text-lg font-bold">{name}</h2>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              {role}
-            </p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">{role}</p>
           </div>
         </div>
         <p className="text-sm text-gray-500 dark:text-gray-400">
